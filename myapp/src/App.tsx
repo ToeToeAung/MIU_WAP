1fdcab958093fb28d8a43a8a0de234113ee03626
Toe Toe Aung <toetoeag@gmail.com>
2024-09-18 23:22:15 -0500

    MIU WAP Reactjs

---
+++ myapp/src/App.tsx
@@ -0,0 +1,28 @@
+import React,{useState} from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let [count,setCount] = useState(0)
+  const increment = () => {
+    setCount( prev => prev+1);
+  }
+
+  const decrement = () => {
+    setCount(count - 1);
+  }
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <hr/>  
+      <div>
+      <button onClick={increment}>+</button>       
+      <button onClick={decrement}>-</button>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
