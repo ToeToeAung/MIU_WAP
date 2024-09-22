74ec89378e9ff3cb8dd71f9427d442637a0fa72d
Toe Toe Aung <toetoeag@gmail.com>
2024-09-22 10:15:15 -0500

    WAP Page Not Found Test

--- Express-Tutorial/src/app.js
+++ Express-Tutorial/src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import path from 'path'; 
+import { fileURLToPath } from 'url'
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);  // Get the filename of the current module
+const __dirname = path.dirname(__filename); 
 
 app.use('/errorTest',(req,res,next)=>{
     throw new Error('Whoops');
@@ -10,5 +14,8 @@ app.use((err,req,res,next)=>{
     res.status(500).send(`Something broke! ${err.message}`);
 })
 
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    });
 
 app.listen(3000,() => {console.log('Server is running at 3000')});
